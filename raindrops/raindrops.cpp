0f5b4619f78078d3e1619de1c7919ec354cfa05c
Qubi <kuba1102@gmail.com>
2024-04-09 19:13:38 +0200

    Add freelancer-rates exercise files and tests

--- raindrops/raindrops.cpp
+++ raindrops/raindrops.cpp
@@ -1,5 +1,28 @@
 #include "raindrops.h"
+#include <string>
+
 
 namespace raindrops {
-
+    std::string convert (int number) {
+        std::string answer;
+        bool flag = false;
+        if (number % 3 == 0) {
+            answer += "Pling";
+            flag = true;
+        } 
+        if (number % 5 == 0) {
+            answer += "Plang";
+            flag = true;
+        } 
+        if (number % 7 == 0) {
+            answer += "Plong";
+            flag = true;
+        } 
+        if (flag == false ){
+            return std::to_string(number);
+        } 
+        else {
+            return answer;
+        }
+    }
 }  // namespace raindrops

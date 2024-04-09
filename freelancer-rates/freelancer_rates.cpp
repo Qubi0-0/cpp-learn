de60c8aafea17a0cdd38cf8e1553cddbd4e0fad6
Qubi <kuba1102@gmail.com>
2024-04-09 20:02:03 +0200

    Implement missing functions and fix bugs in freelancer-rates.cpp

--- freelancer-rates/freelancer_rates.cpp
+++ freelancer-rates/freelancer_rates.cpp
@@ -1,32 +1,29 @@
 // INFO: Headers from the standard library should be inserted at the top via
 // #include <LIBRARY_NAME>
-
+#include <cmath> // Include the <cmath> library for std::ceil and std::floor
 // daily_rate calculates the daily rate given an hourly rate
 double daily_rate(double hourly_rate) {
-    // TODO: Implement a function to calculate the daily rate given an hourly
-    // rate
-    return 0.0;
+    
+    return hourly_rate * 8;
 }
 
 // apply_discount calculates the price after a discount
 double apply_discount(double before_discount, double discount) {
     // TODO: Implement a function to calculate the price after a discount.
-    return 0.0;
+    return  before_discount - (before_discount * 0.01 * discount );
 }
 
 // monthly_rate calculates the monthly rate, given an hourly rate and a discount
-// The returned monthly rate is rounded up to the nearest integer.
-int monthly_rate(double hourly_rate, double discount) {
-    // TODO: Implement a function to calculate the monthly rate, and apply a
-    // discount.
-    return 0;
+int monthly_rate (double hourly_rate, double discount) {
+    double monthly_before = hourly_rate * 8.0 * 22.0;
+    return std::ceil(monthly_before - (monthly_before * 0.01 * discount));
 }
 
-// days_in_budget calculates the number of workdays given a budget, hourly rate,
-// and discount The returned number of days is rounded down (take the floor) to
-// the next integer.
+
+
+
 int days_in_budget(int budget, double hourly_rate, double discount) {
-    // TODO: Implement a function that takes a budget, an hourly rate, and a
-    // discount, and calculates how many complete days of work that covers.
-    return 0;
+    double days = budget / ((hourly_rate - (0.01 * discount * hourly_rate)) * 8);
+    
+    return std::floor(days); // Use std::floor to round down the number of days
 }

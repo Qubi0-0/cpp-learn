e81b347bd9b48f70a5a1f1f94f22bf670ec32e38
Qubi <kuba1102@gmail.com>
2024-04-11 10:34:02 +0200

    Refactor protein_translation.cpp and protein_translation.h, and fix test cases in protein_translation_test.cpp

--- protein-translation/protein_translation.cpp
+++ protein-translation/protein_translation.cpp
@@ -1,5 +1,43 @@
 #include "protein_translation.h"
+#include <vector>
+#include <string>
+
 
 namespace protein_translation {
-
+    std::vector<std::string> proteins(std::string sequence) {
+        std::vector<std::string> result;
+    // int seq_num = sequence.length() / 3;
+        for(size_t i=0;i<sequence.length();i+=3) {
+            std::string codon = sequence.substr(i, 3);
+            if (codon == "AUG") {
+                result.emplace_back("Methionine");
+            } else if (codon == "UUU" || codon == "UUC") {
+                result.emplace_back("Phenylalanine");
+            } else if (codon == "UUA" || codon == "UUG") {
+                result.emplace_back("Leucine");
+            } else if (codon == "UCU" || codon == "UCC" || codon == "UCA" || codon == "UCG") {
+                result.emplace_back("Serine");
+            } else if (codon == "UAU" || codon == "UAC") {
+                result.emplace_back("Tyrosine");
+            } else if (codon == "UGU" || codon == "UGC") {
+                result.emplace_back("Cysteine");
+            } else if (codon == "UGG") {
+                result.emplace_back("Tryptophan");
+            } else {
+                break;
+            }
+        }
+        return result;
+    }
 }  // namespace protein_translation
+
+// | Codon              | Protein       |
+// | :----------------- | :------------ |
+// | AUG                | Methionine    |
+// | UUU, UUC           | Phenylalanine |
+// | UUA, UUG           | Leucine       |
+// | UCU, UCC, UCA, UCG | Serine        |
+// | UAU, UAC           | Tyrosine      |
+// | UGU, UGC           | Cysteine      |
+// | UGG                | Tryptophan    |
+// | UAA, UAG, UGA      | STOP          |
--- protein-translation/protein_translation_test.cpp
+++ protein-translation/protein_translation_test.cpp
@@ -16,7 +16,7 @@ TEST_CASE("Methionine_RNA_sequence")
     REQUIRE(vector<string>{"Methionine"} == protein_translation::proteins("AUG"));
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
+// #if defined(EXERCISM_RUN_ALL_TESTS)
 
 TEST_CASE("Phenylalanine_RNA_sequence_1")
 {
@@ -131,4 +131,4 @@ TEST_CASE("Translation_stops_if_STOP_codon_in_middle_of_six-codon_sequence")
                         == protein_translation::proteins("UGGUGUUAUUAAUGGUUU"));
 }
 
-#endif // !EXERCISM_RUN_ALL_TESTS
+// #endif // !EXERCISM_RUN_ALL_TESTS
